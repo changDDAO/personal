5f1887c2cbeb9b0da96a012255a083a5090c9b9c
Changhogod <younch8342@naver.com>
2023-03-24 15:58:26 +0900

    make search with title, content

--- src/main/java/com/changddao/personal/controller/BoardController.java
+++ src/main/java/com/changddao/personal/controller/BoardController.java
@@ -3,6 +3,10 @@ package com.changddao.personal.controller;
 import com.changddao.personal.entity.Board;
 import com.changddao.personal.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -17,8 +21,13 @@ public class BoardController {
     private BoardRepository boardRepository;
 
     @GetMapping("/list")
-    public String list(Model model) {
-        List<Board> boards = boardRepository.findAll();
+    public String list(Model model, @PageableDefault(size = 2) Pageable pageable, @RequestParam(required = false, defaultValue = "") String searchText) {
+//        Page<Board> boards = boardRepository.findAll(pageable);
+        Page<Board> boards = boardRepository.findByTitleContainingOrContentContaining(searchText, searchText, pageable);
+        int startPage = Math.max(1, boards.getPageable().getPageNumber() - 4);
+        int endPage = Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber()+4);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
         model.addAttribute("boards", boards);
         return "board/list";
     }
